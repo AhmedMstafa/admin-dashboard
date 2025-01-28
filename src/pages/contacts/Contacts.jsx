ea0e774f59aa242a0ff4680da8fed787b4e5e8b3
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-01-28 16:17:25 +0200

    Add contacts page

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,3 +1,16 @@
+import { Box } from '@mui/material';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { columns, rows } from './data';
+
 export default function Contacts() {
-  return <div>Contacts Page Will Be Here</div>;
+  return (
+    <Box sx={{ height: 600, width: '98%', mx: 'auto' }}>
+      <DataGrid
+        slots={{ toolbar: GridToolbar }}
+        rows={rows}
+        // @ts-ignore
+        columns={columns}
+      />
+    </Box>
+  );
 }
